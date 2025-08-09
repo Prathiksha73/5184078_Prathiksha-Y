ce3464914b56bd4cc9dcfd444734998cca69a3a3
Prathiksha Y <119153734+Prathiksha73@users.noreply.github.com>
2025-08-09 19:29:39 +0530

    Update P10-Permuting Two Arrays.c

---
+++ HackerRank/Week1/P10-Permuting Two Arrays.c
@@ -0,0 +1,200 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+
+int compare_asc(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int compare_desc(const void* a, const void* b) {
+    return (*(int*)b - *(int*)a);
+}
+
+char* twoArrays(int k, int A_count, int* A, int B_count, int* B) {
+    static char yes[] = "YES";
+    static char no[] = "NO";
+
+    qsort(A, A_count, sizeof(int), compare_asc);
+    qsort(B, B_count, sizeof(int), compare_desc);
+
+    for (int i = 0; i < A_count; i++) {
+        if (A[i] + B[i] < k) {
+            return no;
+        }
+    }
+    return yes;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* q_endptr;
+    char* q_str = readline();
+    int q = strtol(q_str, &q_endptr, 10);
+
+    if (q_endptr == q_str || *q_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        char** first_multiple_input = split_string(readline());
+
+        char* n_endptr;
+        char* n_str = first_multiple_input[0];
+        int n = strtol(n_str, &n_endptr, 10);
+
+        if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char* k_endptr;
+        char* k_str = first_multiple_input[1];
+        int k = strtol(k_str, &k_endptr, 10);
+
+        if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char** A_temp = split_string(readline());
+
+        int* A = malloc(n * sizeof(int));
+
+        for (int i = 0; i < n; i++) {
+            char* A_item_endptr;
+            char* A_item_str = A_temp[i];
+            int A_item = strtol(A_item_str, &A_item_endptr, 10);
+
+            if (A_item_endptr == A_item_str || *A_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            A[i] = A_item;
+        }
+
+        char** B_temp = split_string(readline());
+
+        int* B = malloc(n * sizeof(int));
+
+        for (int i = 0; i < n; i++) {
+            char* B_item_endptr;
+            char* B_item_str = B_temp[i];
+            int B_item = strtol(B_item_str, &B_item_endptr, 10);
+
+            if (B_item_endptr == B_item_str || *B_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            B[i] = B_item;
+        }
+
+        char* result = twoArrays(k, n, A, n, B);
+
+        fprintf(fptr, "%s\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
